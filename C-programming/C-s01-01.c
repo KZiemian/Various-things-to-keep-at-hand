0a26975347707da246d550adb0e84667bb28759e
Kamil Ziemian <kziemianfvt@gmail.com>
2022-08-08 23:55:23 +0200

    Dodano plik z uwagami do książki „Pro Git”. On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	nowy plik:     C-programming/C-s01-01.c
    	nowy plik:     C-programming/C-s01-02.c
    	nowy plik:     C-programming/C-s01-03.c
    	nowy plik:     C-programming/C-s01-04.c
    	nowy plik:     C-programming/C-s01-05.c
    	nowy plik:     C-programming/C-s01-06.c
    ETC.

---
+++ C-programming/C-s01-01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int c = 0;
+
+
+  printf("Enter a value: ");
+  c = getchar();
+
+
+  printf("\nYou entered:");
+  putchar(c);
+  printf("\n");
+
+
+
+  return 0;
+}
