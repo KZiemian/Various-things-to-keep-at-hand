1af8cb3446b31ed29410468ac09423e129185df1
Kamil Ziemian <kziemianfvt@gmail.com>
2022-10-01 22:29:21 +0200

    Drobne rzeczy

--- Go-programming/Go-s11-75.go
+++ Go-programming/Go-s11-75.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func sumSlice(sliceArg []float32) float32 {
-	var sum float32 = 0.0
+// func sumSlice(sliceArg []float32) float32 {
+// 	var sum float32 = 0.0
 
-	for _, v := range sliceArg {
-		sum += v
-	}
+// 	for _, v := range sliceArg {
+// 		sum += v
+// 	}
 
-	return sum
-}
+// 	return sum
+// }
 
 func main() {
 	// sierpienSlice := []float32{30, 30, 30, 27, 30, 30, 33, 27, 24,
@@ -21,20 +21,20 @@ func main() {
 	// fmt.Printf("Liczba rzeczy zrobionych w sierpnia: %v.",
 	// 	sierpienThingsDone)
 
-	absolPrawdopoLyonsSlice := []float32{0.070, 0.063, 0.052, 0.030,
-		0.026, 0.026, 0.025, 0.019, 0.017, 0.016, 0.016, 0.015}
-	wNaglosiePrawdopoSlice := []float32{0.072, 0.042, 0.034, 0.037,
-		0.065, 0.058, 0.046, 0.010, 0.044, 0.061, 0.020, 0.027}
-	wWyglosiePrawdopoSlice := []float32{0.105, 0.127, 0.034, 0.039,
-		0.0, 0.036, 0.014, 0.048, 0.010, 0.0005, 0.008, 0.002}
+	// absolPrawdopoLyonsSlice := []float32{0.070, 0.063, 0.052, 0.030,
+	// 	0.026, 0.026, 0.025, 0.019, 0.017, 0.016, 0.016, 0.015}
+	// wNaglosiePrawdopoSlice := []float32{0.072, 0.042, 0.034, 0.037,
+	// 	0.065, 0.058, 0.046, 0.010, 0.044, 0.061, 0.020, 0.027}
+	// wWyglosiePrawdopoSlice := []float32{0.105, 0.127, 0.034, 0.039,
+	// 	0.0, 0.036, 0.014, 0.048, 0.010, 0.0005, 0.008, 0.002}
 
-	absolPrawdopoSum := sumSlice(absolPrawdopoLyonsSlice)
-	wNaglosiePrawdopoSum := sumSlice(wNaglosiePrawdopoSlice)
-	wWyglosiePrawdopoSum := sumSlice(wWyglosiePrawdopoSlice)
+	// absolPrawdopoSum := sumSlice(absolPrawdopoLyonsSlice)
+	// wNaglosiePrawdopoSum := sumSlice(wNaglosiePrawdopoSlice)
+	// wWyglosiePrawdopoSum := sumSlice(wWyglosiePrawdopoSlice)
 
-	fmt.Printf("Suma prawdopodobieństw wystąpienia spółgłosek: %v.\n",
-		absolPrawdopoSum)
-	fmt.Printf("Suma prawdopodobieństw wystąpienia spółgosek w nagłosie: %v.\n",
-		wNaglosiePrawdopoSum)
-	fmt.Printf("Sum prawodpodobieństwa wystąpienia spółgłosek w wygłosie: %v.\n", wWyglosiePrawdopoSum)
+	// fmt.Printf("Suma prawdopodobieństw wystąpienia spółgłosek: %v.\n",
+	// 	absolPrawdopoSum)
+	// fmt.Printf("Suma prawdopodobieństw wystąpienia spółgosek w nagłosie: %v.\n",
+	// 	wNaglosiePrawdopoSum)
+	// fmt.Printf("Sum prawodpodobieństwa wystąpienia spółgłosek w wygłosie: %v.\n", wWyglosiePrawdopoSum)
 }
