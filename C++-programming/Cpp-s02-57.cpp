f60132649dc6176bc188776a8c80bd95fa84bc82
Kamil Ziemian <kziemianfvt@gmail.com>
2022-12-03 23:44:45 +0100

    Porządki

---
+++ C++-programming/Cpp-s02-57.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  constexpr double pi = 3.1415926535;
+  constexpr double area = 4 * pi;
+
+  std::cout << "pi: " << pi << '\n';
+  std::cout << "area: " << area << '\n';
+
+  // pi += 1.0;
+
+
+
+  return 0;
+}
