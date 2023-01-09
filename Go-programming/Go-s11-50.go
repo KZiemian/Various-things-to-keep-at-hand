b2dd8801e802c319bd4ed1f98175c4db276a8be0
Kamil Ziemian <kziemianfvt@gmail.com>
2023-01-09 21:43:08 +0100

    English words 02

--- Go-programming/Go-s11-50.go
+++ Go-programming/Go-s11-50.go
@@ -1,9 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	// fmt.Printf("2.0 / 3 = %v\n", 2.0/3)
+
+	// powerOf2 := 1
+
+	// fmt.Printf("2^0 = 1\n")
+
+	// for i := 1; i <= 20; i++ {
+	// 	powerOf2 *= 2
+
+	// 	fmt.Printf("2^%v = %v\n", i, powerOf2)
+	// }
+
+	// factorValue := 1
+
+	// fmt.Println("0! = 1")
+
+	// for i := 1; i <= 10; i++ {
+	// 	factorValue *= i
+
+	// 	fmt.Printf("%v! = %v\n", i, factorValue)
+	// }
+
+	fmt.Printf("5.0 / 9 = %v\n", 5.0/9)
+	fmt.Printf("1.0 / 9 = %v\n", 1.0/9)
+
+	// fmt.Printf("1000.0 / 8 = %v", 1000.0/8)
+	fmt.Printf("1024.0 / 8 = %v\n", 1024.0/8)
 }
