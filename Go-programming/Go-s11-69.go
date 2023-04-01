ae99ff0a9d1537ecb3585b48273232f9003ff5fd
Kamil Ziemian <kziemianfvt@gmail.com>
2023-04-01 22:13:05 +0200

    Dodano README.md

---
+++ Go-programming/Go-s11-69.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Printf("1.0/60.0 = %v.\n", 1.0/60.0)
+	// fmt.Printf("10.0/36.0 = %v.\n", 10.0/36.0)
+	// fmt.Printf("0.28 * 3.6 = %v.\n", 0.28 * 3.6)
+	// fmt.Printf("0.278 * 3.6 = %v.\n", 0.278 * 3.6)
+	fmt.Printf("0.5 * 2.78 * 25 = %v.\n", 0.5 * 2.78 * 25)
+}
---
+++ Go-programming/GoNum/go.mod
@@ -0,0 +1,3 @@
+module example.com/TestGoNum
+
+go 1.20
