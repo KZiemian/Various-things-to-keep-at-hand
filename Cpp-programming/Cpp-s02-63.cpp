410360dd4c821abf87d93c51def3373b52d0f370
Kamil Ziemian <kziemianfvt@gmail.com>
2024-02-17 16:28:08 +0100

    Książki do kupienia, usunięte

--- Cpp-programming/Cpp-s02-63.cpp
+++ Cpp-programming/Cpp-s02-63.cpp
@@ -12,3 +12,26 @@
 
 //   return data;
 // }
+
+#include <iostream>
+
+int main() {
+  // int&& rValueRefInt1 = 10;
+
+  // std::cout << "rValueRefInt1 = " << rValueRefInt1 << ".\n";
+
+  std::string stringVar1 = "string one";
+  std::string stringVar2 = "a really long str";
+
+  // std::cout << "stringVar1 = " << stringVar1 << ".\n";
+
+  std::string&& stringVar3 = stringVar1 + " " + stringVar2;
+
+  std::cout << "stringVar3 = " << stringVar3 << ".\n";
+
+
+
+
+
+  return 0;
+}
