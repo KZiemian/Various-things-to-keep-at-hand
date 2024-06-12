081c262221bb82ab792489b10be35bb375df39b7
Kamil Ziemian <kziemianfvt@gmail.com>
2024-06-12 23:36:35 +0200

    Ruby programming 03

---
+++ Ruby-programming/Ruby-s01-03.rb
@@ -0,0 +1,112 @@
+#!/home/kamil/.rbenv/shims/ruby
+
+# age = 10
+
+# unless age >= 18
+#   puts "Sorry, you need to be at least eighteen to drive car. Grow up fast!"
+# end
+
+# def check_sign(number)
+#   number > 0 ? "#{number} is positive" : "#{number} is negative"
+# end
+
+# puts check_sign(1)
+# puts check_sign(0)
+# puts check_sign(-1)
+
+# if 0
+#   puts "Hey, 0 is considered to be a truth in Ruby"
+# end
+
+# loop do
+#   monk.meditate
+
+#   if monk.nirvana?
+#     break
+#   end
+# end
+
+# 5.times do
+#   puts "Do"
+# end
+
+# bell1 = "bell"
+
+# def ring(bell, n)
+#   n.times do
+#     puts "bell rings"
+#   end
+# end
+
+# ring(bell1, 2)
+# ring(bell1, 5)
+
+# puts []
+# puts Array.new
+
+# puts [1, 2, 3, 4, 5]
+
+# puts [1, 2, 3, 4, 5][2]
+
+# puts [1, 2, 3, 4, 5, 6, 7][4]
+
+# puts [1, 2, 3, 4, 5][-5]
+
+# puts [1, 2, 3, 4, 5][-1]
+
+# puts [1, 2, 3, 4, 5] << "woot"
+
+# puts [1, 2, 3, 4, 5].push("woot")
+
+# puts [1, 2, 3, 4, 5].map { |i| i + 1 }
+
+# puts [1, 2, 3, 4, 5].map { |i| 3*i }
+
+# puts [1, 2, 3, 4, 5, 6].select { |number| number % 2 == 0 }
+
+# names = ["rock", "paper", "scissors", "lizard", "spock"]
+
+# puts names.select { |stringVar| stringVar.length > 5 }
+
+# numbers = [1, 3, 5, 4, 6, 7]
+
+# puts numbers
+
+# puts numbers.delete(5)
+
+# puts numbers
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+
+# puts numbers.delete_if{ |i| i < 4 }
+
+# puts numbers
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# puts numbers
+
+# numbers.delete_if{ |i| i % 2 == 0 }
+
+# puts numbers
+
+# array = [1, 2, 3, 4, 5]
+
+# for i in array
+#   puts i
+# end
+
+# def array_copy(source)
+#   destination = []
+
+#   for elem in source
+#     if elem < 4
+#       destination.push(elem)
+#     end
+#   end
+
+#   return destination
+# end
+
+# puts array_copy([1, 2, 3, 4, 5])
+# puts array_copy([-23, 12, 0, 19])
---
+++ Ruby-programming/Ruby-s01-04.rb
@@ -0,0 +1,22 @@
+#!/home/kamil/.rbenv/shims/ruby
+
+# array = [1, 2, 3, 4, 5]
+
+# array.each do |i|
+#   puts i
+# end
+
+# def array_copy(source)
+#   destination = []
+
+#   source.each do |elem|
+#     if elem < 4
+#       destination.push(elem)
+#     end
+#   end
+
+#   return destination
+# end
+
+# puts array_copy([1, 2, 3, 4, 5])
+# puts array_copy([-23, 12, 0, 19])
