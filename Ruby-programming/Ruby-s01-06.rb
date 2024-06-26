6ee85b2dde3c2e417f38dda2bf462d1a1a7939dc
Kamil Ziemian <kziemianfvt@gmail.com>
2024-06-27 00:09:41 +0200

    Ruby programming 05

--- Ruby-programming/Ruby-s01-06.rb
+++ Ruby-programming/Ruby-s01-06.rb
@@ -1,4 +1,5 @@
 #!/home/kamil/.rbenv/shims/ruby
+# coding: utf-8
 
 # def add(*numbers)
 #   numbers.inject(0) { |sum, number| sum + number }
@@ -22,5 +23,79 @@
 # puts add(1.0134, -5.568, absolute: true)
 # puts add(1.0134, -5.568, absolute: true, round: true, precision: 2)
 
-print "Hello World!"
-puts "Hello World!"
+# print "Hello World!"
+# puts "Hello World!"
+
+# puts "Mike"
+# print "Giraffe Academy"
+# print " is cool"
+
+# puts "   /|"
+# puts "  / |"
+# puts " /  |"
+# puts "/___|"
+
+# character_name = "John"
+# character_age = "35"
+
+# puts "There once was a man named #{character_name}"
+# puts "he was #{character_age} years old."
+# puts "He really liked the name #{character_name}"
+# puts "but didn't like being #{character_age}."
+
+# name = "Mike"
+# occupation = "programmer"
+
+# age = 37
+
+# puts age
+
+# stringVar = "string"
+
+# puts "stringVar = #{stringVar}"
+
+# intVar = 1
+
+# puts "intVar = #{intVar}"
+
+# floatVar = 0.5
+
+# puts "floatVar = #{floatVar}"
+
+# boolVar = true
+
+# puts "boolVar = #{boolVar}"
+
+# nilVar = nil
+
+# puts "nilVar = #{nilVar}"
+
+# puts "Jeden \"Dwa\""
+
+# puts "Jedne\nDwa\nTrzy"
+
+# phrase = "Giraffe Academy"
+
+# puts phrase.upcase()
+
+# phrase = "    Giraffe Academy    "
+
+# puts phrase.strip
+
+phrase = "Giraffe Academy"
+
+# puts phrase.length
+
+# puts phrase.include?("Giraffe")
+
+# puts phrase[0]
+
+# puts phrase[0, 3]
+
+# puts phrase.index("f")
+
+puts 5
+
+puts 5.123
+
+puts 5 + 9
---
+++ Ruby-programming/Ruby-s01-07.rb
@@ -0,0 +1,57 @@
+#!/home/kamil/.rbenv/shims/ruby
+# coding: utf-8
+
+# puts 2**3
+
+# puts "2 + 3 = #{2 + 3}"
+
+# puts "3 - 4 = #{3 - 4}"
+
+# puts "2 * 3 = #{2 * 3}"
+
+# puts "5 / 2 = #{5 / 2}"
+
+# puts "10 % 3 = #{10 % 3}"
+
+# number = -1
+
+# puts number
+
+# puts number.abs
+
+# puts number.abs()
+
+# number = 1.234
+
+# puts number
+
+# puts number.round()
+
+# number = 1.234
+
+# puts number.ceil()
+
+# puts number.floor()
+
+# puts Math.sqrt(2)
+
+# puts 3 / 2
+
+# puts 3.0 / 2
+
+# puts 3 / 2.0
+
+# puts 3.0 / 2.0
+
+# puts "Enetr your name: "
+# name = gets.chomp()
+
+# puts "name -> #{name}, i co jeszcze?"
+
+
+
+
+
+
+# https://www.youtube.com/watch?v=t_ispmWmdjY
+# Skończyłem na 1:01:00.
