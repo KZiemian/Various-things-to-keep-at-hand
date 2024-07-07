ef8c64cec065642298ad7a8cfa66e97150185dd8
Kamil Ziemian <kziemianfvt@gmail.com>
2024-07-07 13:14:30 +0200

    LaTeX symbols 01

--- Ruby-programming/Ruby-s01-09.rb
+++ Ruby-programming/Ruby-s01-09.rb
@@ -16,66 +16,85 @@
 # puts "max(3, 1, 2) = #{max(3, 1, 2)}"
 # puts "max(3, 4, 5) = #{max(3, 4, 5)}"
 
+# puts "[\"rock\", \"paper\", \"scissors\"].index(\"paper\") = #{["rock", "paper", "scissors"].index("paper")}."
+# puts "[\"rock\", \"paper\", \"scissors\"].index(\"rock\") = #{["rock", "paper", "scissors"].index("rock")}"
 
+# puts "2.between?(1, 3) = #{2.between?(1, 3)}"
+# puts "3.between?(1, 3) = #{3.between?(1, 3)}"
 
+# puts "4.+(3) = #{4.+(3)}"
+# puts "4 + 3 = #{4 + 3}"
 
+# words = ["foo", "bar", "baz"]
 
+# puts "words = #{words}"
+# puts words
 
+# puts "words[1] = #{words[1]}"
 
+# puts "words.[](1) = #{words.[](1)}"
 
+# puts "\"Ruby Monk\".length = #{"Ruby Monk".length}"
 
+# a = 1
+# b = 4
 
+# puts "The number #{a} is less than #{b}."
 
+# string_var = "Matz"
 
+# puts "string_var -> #{string_var}"
 
+# def string_length_interpolater(incoming_string)
+#   "The string you just gave me has a length of #{incoming_string.length}"
+# end
 
+# string_var = "Matz"
 
+# puts "string_length_interpolater(#{string_var}) = #{string_length_interpolater(string_var)}"
 
+# string_var = "Pink Floyd"
 
+# puts "string_length_interpolater(#{string_var}) = #{string_length_interpolater(string_var)}"
 
+# string_var = "Rick Beato"
 
+# puts "string_length_interpolater(#{string_var}) = #{string_length_interpolater(string_var)}"
 
+# puts "\"[Luke:] I can't believe it. [Yoda:] That is why you fail.\".include?(\"Yoda\") = #{"[Luke:] I can't believe it. [Yoda:] That is why you fail.".include?("Yoda")}"
 
+# puts "\"Ruby is a beautiful language\".start_with?(\"Ruby\") = #{"Ruby is a beautiful language".start_with?("Ruby")}"
 
+# puts "\"I can't work with any other language but Ruby\".end_with?(\"Ruby\") = #{"I can't work with any other langauge but Ruby".end_with?("Ruby")}"
 
+# puts "\"I am a Rubyist\".index(\"R\") = #{"I am a Rubist".index("R")}"
 
+# puts "\"i am in lowercase\".upcase = #{"i am in lowercase".upcase}"
 
+# puts "\"This is Mixed CASE\".downcase = #{"This is Mixed CASE".downcase}"
 
+# puts "\"ThiS iS A vErY ComPLEx SenTeNcE\".swapcase = #{"ThiS iS A vErY ComPLEx SenTeNce".swapcase}"
 
+# puts "\"Fear is the path to the dark side\".split(\" \") = #{"Fear is the path to the dark side".split(" ")}"
 
+# string1 = "Ruby"
+# string2 = " Monk"
 
+# puts string1 + string2
+# puts "string1 = #{string1}"
 
+# puts string1.concat(string2)
+# puts "string1 = #{string1}"
 
+# puts string1 << string2
+# puts "string1 = #{string1}"
 
+# string1 = "I should look into your problem when I get time"
 
+# puts "string1 -> #{string1}"
+# puts string1.sub("I", "We")
+# puts "string1 -> #{string1}"
 
+# puts string1.gsub("I", "We")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# https://www.youtube.com/watch?v=t_ispmWmdjY
-# Skończyłem na 1:59:00.
+# puts "Ruby Monk".gsub(/[aeiou]/, "1")
---
+++ Ruby-programming/Ruby-s01-10.rb
@@ -0,0 +1,59 @@
+#!/home/kamil/.rbenv/shims/ruby
+# coding: utf-8
+
+# puts "Ruby Monk Is Pretty Brilliant".gsub(/[RMIPB]/, "0")
+
+# puts "Ruby Monk Is Pretty Brilliant".match(/ ./)
+
+# puts "Ruby Monk Is Pretty Brilliant".match(/ ./, 10)
+
+# puts "true and true -> #{true and true}"
+# puts "false and true -> #{false and true}"
+# puts "true and false -> #{true and false}"
+# puts "false and false -> #{false and false}"
+
+# puts "true && true -> #{true && true}"
+# puts "false && true -> #{false && true}"
+# puts "true && false -> #{true && false}"
+# puts "false && false -> #{false && false}"
+
+# puts "true or true -> #{true or true}"
+# puts "false or true -> #{false or true}"
+# puts "true or false -> #{true or false}"
+# puts "false or false -> #{false or false}"
+
+# puts "true || true -> #{true || true}"
+# puts "false || true -> #{false || true}"
+# puts "true || false -> #{true || false}"
+# puts "false || false -> #{false || false}"
+
+# puts "true == true -> #{true == true}"
+# puts "false == true -> #{false == true}"
+# puts "true == false -> #{true == false}"
+# puts "false == false -> #{false == false}"
+
+# puts "1 < 1 -> #{1 < 1}"
+# puts "1 < 2 -> #{1 < 2}"
+# puts "1 > 1 -> #{1 > 1}"
+# puts "2 > 1 -> #{2 > 1}"
+
+# puts "1 <= 1 -> #{1 <= 1}"
+# puts "1 <= 2 -> #{1 <= 2}"
+# puts "2 <= 1 -> #{2 <= 1}"
+
+# puts "1 >= 1 -> #{1 >= 1}"
+# puts "1 >= 2 -> #{1 >= 2}"
+# puts "2 >= 1 -> #{2 >= 1}"
+
+
+
+
+
+
+
+
+
+# https://www.youtube.com/watch?v=t_ispmWmdjY
+# Skończyłem na 1:59:00.
+
+# https://rubymonk.com/learning/books/1-ruby-primer/chapters/8-control-structures/lessons/43-boolean-expressions-in-ruby
